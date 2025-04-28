b9bc0188514bff7b330d0935bd779f12304eb252
aasim <gameaasim931@Gmail.com>
2025-04-28 15:50:02 +0530

    a

---
+++ vive_report.php
@@ -0,0 +1,34 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+<table class="table table-bordered">
+                    <thead class="table-dark">
+                        <tr>
+                     
+                            <th>S.No.</th>
+                            <th class = "th-de">Product Name</th>
+                            <th class="th-me" style="display: none;">Product</th>
+                            <th>Quantity</th>
+                            <th>Total Price</th>
+                            <th>Pay mode</th>
+                        </tr>
+                    </thead>
+                <tbody>
+=     <a href="bill_.html"><--back-pos</a> <select name="" id="" >
+    <option value="Daly report">Daly report</option>
+    <option value="Daly report">mounth report</option></select>
+
+</select>
+
+<style>
+   select {
+    margin-left:80%;
+   }
+@media (max-width: 600px) {
+    select{
+        margin-left: 50%;
+    }
+}
+
+
+</style>
